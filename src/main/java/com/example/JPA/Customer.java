275f4b18d04e4306bf0172bf7b33cdb101136766
Anand Shastri <anandshastri23@gmail.com>
2018-03-18 12:22:29 -0500

    create sample project using jpa, connects to db and fetches records based on query

---
+++ src/main/java/com/example/JPA/Customer.java
@@ -0,0 +1,33 @@
+package com.example.JPA;
+
+import javax.persistence.*;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="firstName")
+    private String firstName;
+
+    @Column(name="lastName")
+    private String lastName;
+
+    protected Customer() {}
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[id=%d, firstName='%s', lastName='%s']",
+                id, firstName, lastName);
+    }
+
+}
